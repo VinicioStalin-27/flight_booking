88e556f57f1eaff1f6eee84edeb184f4da2baa85
Vinicio Salcedo <vinicio.salcedo@gmail.com>
2025-03-16 12:47:47 -0500

    remove secret keys from repo

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 
-TELEGRAM_BOT_TOKEN = "7885142341:AAHty481L_62iiM9_sgV5PM4DJTJPUEiZ-4"  # Reemplaza con tu token real
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:vsphome@localhost/flight_booking"
+TELEGRAM_BOT_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # Reemplazar con token real
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://user:password@localhost/flight_booking"
